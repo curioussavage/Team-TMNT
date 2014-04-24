5f46128f036ab56c13cf374a60187fae98143884
Adam Winn <adambwinn@gmail.com>
2014-04-23 20:56:39 -0600

    add state provide

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,34 @@
+//'use strict';
+//
+//angular
+//  .module('teamTmntApp', [
+//    'ngCookies',
+//    'ngResource',
+//    'ngSanitize'
+//  ]);
+
+
 'use strict';
 
-angular
-  .module('teamTmntApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize'
-  ]);
+angular.module('teamTmntApp', [
+    'ui.router'
+]).config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('main', {
+        url: '/',
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+    })
+        .state('team', {
+            url: '/team/:teamId',
+            templateUrl: 'views/team.html',
+            controller: 'TeamCtrl',
+//            resolve: {
+//                team: function (TurtleService, $stateParams) {
+//                    return TurtleService.getTeam($stateParams.teamId);
+//                }
+            }
+        });
+
+});
